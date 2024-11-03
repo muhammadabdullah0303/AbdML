5a926d932898b394ea5100c9da13a251fa046d54
Muhammad Abdullah <mrabdullah0303@gmail.com>
2024-11-03 11:24:26 -0800

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+%%time
+
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -18,7 +20,7 @@ class AbdBase:
     
     def __init__(self, train_data, test_data=None, target_column=None,
                  problem_type="classification", metric="roc_auc", seed=SEED,
-                 n_splits=5, cat_features=None, num_classes=None, prob=True): 
+                 n_splits=5, cat_features=None, num_classes=None, prob=True, early_stop=False): 
         self.train_data = train_data
         self.test_data = test_data
         self.target_column = target_column
@@ -29,6 +31,7 @@ class AbdBase:
         self.cat_features = cat_features if cat_features else []
         self.num_classes = num_classes
         self.prob = prob 
+        self.early_stop = early_stop
 
         self._validate_input()
         self.checkTarget()
@@ -49,7 +52,9 @@ class AbdBase:
         print("Available Problem Types:", ", ".join(self.problem_types))
         print(f"Problem Type Selected: {self.problem_type}")
         print(f"Metric Selected: {self.metric}")
-        print(f"Calculate Predicted Probabilities: {self.prob}")  # <-- Display 'prob'
+        print(f"Calculate Predicted Probabilities: {self.prob}")
+        print(f"Early Stopping : {self.early_stop}")
+
 
     def _validate_input(self):
         if not isinstance(self.train_data, pd.DataFrame):
@@ -82,8 +87,8 @@ class AbdBase:
             return r2_score(y_true, y_pred)
         else:
             raise ValueError(f"Unsupported metric '{self.metric}'")
-            
-    def LGBM(self, params):
+
+    def Train_ML(self, params, model_name):
         if self.metric not in ["roc_auc", "accuracy"]:
             raise ValueError(f"Metric '{self.metric}' is not supported. Choose from: 'roc_auc', 'accuracy'.")
         if self.problem_type not in ["classification", "regression"]:
@@ -99,15 +104,28 @@ class AbdBase:
             np.zeros((len(self.X_test), self.n_splits))
         )
 
+        cat_features_indices = [self.X_train.columns.get_loc(col) for col in self.cat_features] if model_name == 'CAT' else None
+
         for fold, (train_idx, val_idx) in enumerate(tqdm(kfold.split(self.X_train, self.y_train), desc="Training Folds", total=self.n_splits)):
             X_train, X_val = self.X_train.iloc[train_idx], self.X_train.iloc[val_idx]
             y_train, y_val = self.y_train.iloc[train_idx], self.y_train.iloc[val_idx]
 
-            model = lgb.LGBMClassifier(**params,random_state=self.seed,verbose=-1) if self.problem_type == 'classification' else lgb.LGBMRegressor(**params)
-            model.fit(X_train, y_train, eval_set=[(X_val, y_val)])
+            if model_name == 'LGBM':
+                model = lgb.LGBMClassifier(**params, random_state=self.seed, verbose=-1) if self.problem_type == 'classification' else lgb.LGBMRegressor(**params)
+            elif model_name == 'CAT':
+                train_pool = Pool(data=X_train, label=y_train, cat_features=cat_features_indices)
+                val_pool = Pool(data=X_val, label=y_val, cat_features=cat_features_indices)
+                model = CatBoostClassifier(**params, random_state=self.seed, verbose=0) if self.problem_type == 'classification' else CatBoostRegressor(**params)
+            else:
+                raise ValueError("model_name must be 'LGBM' or 'CAT'.")
+
+            if model_name == 'LGBM':
+                model.fit(X_train, y_train, eval_set=[(X_val, y_val)])
+            elif model_name == 'CAT':
+                model.fit(train_pool, eval_set=val_pool, early_stopping_rounds=50 if self.early_stop==True else None )
 
             if self.problem_type == 'classification':
-                y_train_pred = model.predict_proba(X_train)[:, 1] if self.prob else model.predict(X_train)  
+                y_train_pred = model.predict_proba(X_train)[:, 1] if self.prob else model.predict(X_train)
                 y_val_pred = model.predict_proba(X_val)[:, 1] if self.prob else model.predict(X_val)
             else:
                 y_train_pred = model.predict(X_train)
@@ -123,64 +141,10 @@ class AbdBase:
                 oof_scores.append(roc_auc_score(y_val, y_val_pred, multi_class="ovr" if self.num_classes > 2 else None))
 
             if self.X_test is not None:
-                test_preds[:, fold] = model.predict_proba(self.X_test)[:, 1] if self.prob else model.predict(self.X_test) 
-
-            print(f"Fold {fold + 1} - Train {self.metric.upper()}: {train_scores[-1]:.4f}, OOF {self.metric.upper()}: {oof_scores[-1]:.4f}")
-            clear_output(wait=True)
-
-        print(f"Overall Train {self.metric.upper()}: {np.mean(train_scores):.4f}")
-        print(f"Overall OOF {self.metric.upper()}: {np.mean(oof_scores):.4f}")
-
-        mean_test_preds = test_preds.mean(axis=1) if self.X_test is not None else None
-        return oof_predictions, mean_test_preds
-
-    def CAT(self, params):
-        if self.metric not in ["roc_auc", "accuracy"]:
-            raise ValueError(f"Metric '{self.metric}' is not supported. Choose from: 'roc_auc', 'accuracy'.")
-        if self.problem_type not in ["classification", "regression"]:
-            raise ValueError(f"Problem type '{self.problem_type}' is not supported. Choose from: 'classification', 'regression'.")
-
-        kfold = StratifiedKFold(n_splits=self.n_splits, shuffle=True, random_state=self.seed)
-        train_scores = []
-        oof_scores = []
-        oof_predictions = np.zeros((len(self.y_train), self.num_classes)) if self.num_classes > 2 else np.zeros(len(self.y_train))
-        test_preds = (
-            None if self.X_test is None else
-            np.zeros((len(self.X_test), self.n_splits, self.num_classes)) if self.num_classes > 2 else
-            np.zeros((len(self.X_test), self.n_splits))
-        )
-
-        cat_features_indices = [self.X_train.columns.get_loc(col) for col in self.cat_features]
-
-        for fold, (train_idx, val_idx) in enumerate(tqdm(kfold.split(self.X_train, self.y_train), desc="Training Folds", total=self.n_splits)):
-            X_train, X_val = self.X_train.iloc[train_idx], self.X_train.iloc[val_idx]
-            y_train, y_val = self.y_train.iloc[train_idx], self.y_train.iloc[val_idx]
-        
-            train_pool = Pool(data=X_train, label=y_train, cat_features=cat_features_indices)
-            val_pool = Pool(data=X_val, label=y_val, cat_features=cat_features_indices)
-        
-            model = CatBoostClassifier(**params, random_state=self.seed, verbose=0)
-            
-            model.fit(train_pool, eval_set=val_pool, early_stopping_rounds=50)
-
-            if self.problem_type == 'classification':
-                y_train_pred = model.predict_proba(X_train)[:, 1] if self.prob else model.predict(X_train)  
-                y_val_pred = model.predict_proba(X_val)[:, 1] if self.prob else model.predict(X_val)
-            else:
-                y_train_pred = model.predict(X_train)
-                y_val_pred = model.predict(X_val)
-
-            oof_predictions[val_idx] = y_val_pred
-
-            if self.metric == "accuracy":
-                train_scores.append(accuracy_score(y_train, np.argmax(y_train_pred, axis=1) if self.num_classes > 2 else (y_train_pred > 0.5).astype(int)))
-                oof_scores.append(accuracy_score(y_val, np.argmax(y_val_pred, axis=1) if self.num_classes > 2 else (y_val_pred > 0.5).astype(int)))
-            elif self.metric == "roc_auc":
-                train_scores.append(roc_auc_score(y_train, y_train_pred, multi_class="ovr" if self.num_classes > 2 else None))
-                oof_scores.append(roc_auc_score(y_val, y_val_pred, multi_class="ovr" if self.num_classes > 2 else None))
-
-            if self.X_test is not None:
-                test_preds[:, fold] = model.predict_proba(self.X_test)[:, 1] if self.prob else model.predict(self.X_test)
+                if self.problem_type == 'classification':
+                    test_preds[:, fold] = model.predict_proba(self.X_test)[:, 1] if self.prob else model.predict(self.X_test)
+                else:
+                    test_preds[:, fold] = model.predict(self.X_test)
 
             print(f"Fold {fold + 1} - Train {self.metric.upper()}: {train_scores[-1]:.4f}, OOF {self.metric.upper()}: {oof_scores[-1]:.4f}")
             clear_output(wait=True)
