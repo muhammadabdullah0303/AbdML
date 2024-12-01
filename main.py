e621baa52573d33f437a87d9eeb2a0de36b02703
Muhammad Abdullah <mrabdullah0303@gmail.com>
2024-12-01 11:15:00 -0800

    main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class AbdBase:
     current_version = ['V_1.3']
     
     def __init__(self, train_data, test_data=None, target_column=None,tf_vec=False,gpu=False,numpy_data=False,
-                 problem_type="classification", metric="roc_auc", seed=SEED,
+                 problem_type="classification", metric="roc_auc", seed=SEED,log_y=False,
                  n_splits=5, cat_features=None, num_classes=None, prob=False,stat_fe = None,logger: Optional[logging.Logger] = None,
                  early_stop=False, test_prob=False, fold_type='SKF',weights=None,multi_column_tfidf=None):
 
@@ -64,6 +64,7 @@ class AbdBase:
         self.gpu = gpu
         self.numpy_data = numpy_data
         self.logger = logger or self._setup_default_logger()
+        self.log_y = log_y
         
         self._validate_input()
         self.checkTarget()
@@ -125,6 +126,7 @@ class AbdBase:
         self.X_train = self.train_data.drop(self.target_column, axis=1).to_numpy() if self.numpy_data else self.train_data.drop(self.target_column, axis=1)
         self.y_train = self.train_data[self.target_column].to_numpy() if self.numpy_data else self.train_data[self.target_column]
         self.y_train = self.y_train.reshape(-1, 1) if self.model_name == 'TABNET' else self.y_train
+        self.y_train = np.log1p(self.y_train) if self.log_y else None
         
         if self.test_data is not None:
             self.X_test = self.test_data.to_numpy() if self.numpy_data else self.test_data
@@ -351,7 +353,14 @@ class AbdBase:
             else:
                 y_train_pred = model.predict(X_train)
                 y_val_pred = model.predict(X_val)
-
+                
+            if self.log_y:
+                y_train_pred = np.expm1(y_train_pred)
+                y_val_pred = np.expm1(y_val_pred)
+            else :
+                y_train_pred = y_train_pred
+                y_val_pred = y_val_pred
+                
             oof_predictions[val_idx] = y_val_pred
 
             if self.metric == "accuracy":
@@ -389,6 +398,10 @@ class AbdBase:
         print(f"Overall OOF {self.metric.upper()}:{mean_off_scores} ") if optuna == False else None
 
         mean_test_preds = test_preds.mean(axis=1) if self.X_test is not None else None
+        if self.log_y:
+            mean_test_preds = np.expm1(mean_test_preds)
+        else :
+            mean_test_preds = mean_test_preds
         return oof_predictions, mean_test_preds , model , all_models , mean_off_scores , mean_train_scores
     
     def _setup_default_logger(self) -> logging.Logger:
