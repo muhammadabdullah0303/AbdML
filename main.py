7b401f0e9c4921a6dd67f7d5b63ffc42093db001
Muhammad Abdullah <mrabdullah0303@gmail.com>
2024-12-05 21:53:33 -0800

    main.py

--- main.py
+++ main.py
@@ -131,7 +131,32 @@ class AbdBase:
         else:
             self.X_test = None
 
-                
+
+    def CIBMTR_score(self,solution: pd.DataFrame, submission: pd.DataFrame, row_id_column_name: str) -> float:
+        
+        del solution[row_id_column_name]
+        del submission[row_id_column_name]
+        
+        event_label = 'efs'
+        interval_label = 'efs_time'
+        prediction_label = 'prediction'
+        for col in submission.columns:
+            if not pandas.api.types.is_numeric_dtype(submission[col]):
+                raise ParticipantVisibleError(f'Submission column {col} must be a number')
+        merged_df = pd.concat([solution, submission], axis=1)
+        merged_df.reset_index(inplace=True)
+        merged_df_race_dict = dict(merged_df.groupby(['race_group']).groups)
+        metric_list = []
+        for race in merged_df_race_dict.keys():
+            indices = sorted(merged_df_race_dict[race])
+            merged_df_race = merged_df.iloc[indices]
+            c_index_race = concordance_index(
+                            merged_df_race[interval_label],
+                            -merged_df_race[prediction_label],
+                            merged_df_race[event_label])
+            metric_list.append(c_index_race)
+        return float(np.mean(metric_list)-np.sqrt(np.var(metric_list)))
+    
     @staticmethod
     def text_stat(df, txt_cols):
         stop_words = set(stopwords.words('english'))
